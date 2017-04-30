80ca4bc8d5ad2ebb87ad1bf5194d0326223543a0
PunitGr <groove679@gmail.com>
2017-04-30 08:56:56 +0530

    Add slider modal and update design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+jQuery(document).ready(function($){
 
+    $("#demo02").animatedModal({
+            modalTarget:'modal-02',
+            animatedIn:'fadeInUpBig',
+            animatedOut:'fadeOutDownBig',
+            // Callbacks
+            beforeOpen: function() {
+                    console.log("The animation was called");
+            },
+            afterOpen: function() {
+                    console.log("The animation is completed");
+            },
+            beforeClose: function() {
+                    console.log("The animation was called");
+            },
+            afterClose: function() {
+                    console.log("The animation is completed");
+            }
+    });
+});
